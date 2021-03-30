a8ed019e18e19bf67355c12fe83285964795fd42
Yoshibosh <shevtsov2001@gmail.com>
2021-03-30 18:59:04 +0700

    Что вершит судьбу человечества в этом мире? Некое незримое существо или закон, подобно Длани Господней парящей над миром? По крайне мере истинно то, что человек не властен даже над своей волей.

--- neiro.cs
+++ neiro.cs
@@ -8,14 +8,16 @@ namespace Neiro{
     public class Net{
         public static void Neironka(){
 
+            double[] correctValues = new double[10]{0.1,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9};
+
+            Layer.SetNeedValues(correctValues);
+
             Layer[] layers = new Layer[3];
-            WriteLine("1");
             layers[0] = new Layer(225);
-            WriteLine("2");
-            layers[1] = new Layer(200,layers[0]);
-            WriteLine("3");
-            layers[2] = new Layer(10,layers[1]);
-            WriteLine("4");
+            layers[1] = new Layer(200);
+            layers[2] = new Layer(10);
+            
+            WriteLine(" rqweijhropi{0}",layers[2].neurons[0].rightConnections);
             
             int number = 0;
             int variation = 0;
@@ -25,15 +27,23 @@ namespace Neiro{
                 if (number > 9){number = 0;}
                 if (variation > 5){variation = 0;}
                 
-                WriteLine("5");
 
                 layers[0].SetData(GetData(number,variation));
-                WriteLine("6");
 
                 for (int j = 1;j < 3;j++){
                     for (int i = 0;i < layers[j].length;i++){
                         layers[j].neurons[i].Activation();
-                        WriteLine("Layer = {0}\tneuronPosition = {1}\tvalue = {2}",layers[j].neurons[i].layer.currentCount,i,layers[j].neurons[i].value);
+                        if (j == 2){
+                            WriteLine("Layer = {0}\tneuronPosition = {1}\tvalue = {2}",layers[j].currentCount,i,layers[j].neurons[i].value);
+                        }
+                    }
+                }
+                for (int j = 1;j < 3;j++){
+                    for (int i = 0;i < layers[j].length;i++){
+                        layers[j].neurons[i].ErrorCalculation();
+                        if (j == 2){
+                            WriteLine("Layer = {0}\tneuronPosition = {1}\tWrongValue = {2}",layers[j].currentCount,i,layers[j].neurons[i].wrongValue);
+                        }
                     }
                 }
 
@@ -41,6 +51,8 @@ namespace Neiro{
                 variation++;
 
             }
+
+            WriteLine(layers[2].neurons[0].wrongValue);
             
 
 
@@ -49,7 +61,7 @@ namespace Neiro{
         static byte[] GetData(int number,int variation){
             string path = $@"C:\Users\User\Desktop\a\Kod\CS\app\dataset\{number}({variation}).data";
 
-            WriteLine(path);
+            //WriteLine(path);
 
             byte[] data = new byte[675];
             byte[] newData = new byte[225];
@@ -95,65 +107,113 @@ namespace Neiro{
     }
 
     class Layer{
-
+        public static double[] correctValues;
         public static int Count = 0;
-        public int currentCount = 0;
+        public int currentCount;
         public Neuron[] neurons;
         public int length;
-        public Layer previousLayer;
-        public Layer(int length,Layer previousLayer = null){
+        public static Layer previousLayer = null;
+        public Layer prevLayer;
+        public Layer nextLayer;
+        public Layer(int length){
+            Random rand = new Random();
+
+            WriteLine(Count);
+
+            this.prevLayer = previousLayer;            
+            this.neurons = new Neuron[length];
+            this.length = length;
+
+            for (int i = 0;i < length;i++){
+                this.neurons[i] = new Neuron(this);
+                this.neurons[i].count = i;
+            }
+
+            if (previousLayer != null)
+            {
+                for (int i = 0;i < length;i++){
+                    this.neurons[i].leftConnections = new double[this.prevLayer.length];
+                    for (int j = 0; j < prevLayer.length;j++){
+                         this.neurons[i].leftConnections[j] = rand.Next(-1,1)*rand.NextDouble();
+                    }
+                }
+                for (int i = 0;i < this.prevLayer.length;i++){
+                    this.prevLayer.neurons[i].rightConnections = new double[length];
+                    for (int j = 0; j < length;j++){
+                         this.prevLayer.neurons[i].rightConnections[j] = rand.Next(-1,1)*rand.NextDouble();
+                    }
+                }
+
+
+                this.prevLayer.nextLayer = this;
+            }
+
 
             this.currentCount = Count;
             Count++;
 
-            this.previousLayer = previousLayer;
-            this.neurons = new Neuron[length];
-            this.length = length;
-    
-            for (int i = 0;i < length;i++){
-                this.neurons[i] = new Neuron(this);
-            }
+            previousLayer = this;
 
         }
 
         public void SetData(byte[] data){
-            for (int i = 0;i < this.length;i++){
-                neurons[i].value = data[i];
+            if (this.currentCount != 0){
+                throw new Exception("This layer is not first");
             }
+            for (int i = 0;i < this.length;i++){
+                this.neurons[i].value = data[i];
+            }
+        }
+
+        public static void SetNeedValues(double[] values){
+            correctValues = values;
         }
     }
 
     class Neuron{
-        public double[] connections;
+        public double[] leftConnections;
+        public double[] rightConnections = null;
+        public int count;
         public double value;
+        public double wrongValue;
         public Layer layer;
 
         public Neuron(Layer layer){
 
-            Random rand = new Random();
-
             this.layer = layer;
             this.value = 0;
 
-            if (this.layer.previousLayer != null){
-                
-                this.connections = new double[this.layer.previousLayer.length];
-
-                for(int i = 0;i < this.layer.previousLayer.length;i++){
-                    this.connections[i] = rand.Next(-1,1)*rand.NextDouble();
-                }
-            }
         }
 
         public void Activation(){
             double sum = 0;
 
-            WriteLine("Layer = {0}",this.layer.currentCount);
-            for (int i = 0;i < this.layer.previousLayer.length;i++){
-                sum += this.connections[i]*this.layer.previousLayer.neurons[i].value;
+            if (this.layer.prevLayer == null){
+                return;
+            }
+
+            for (int i = 0;i < this.layer.prevLayer.length;i++){
+                sum += this.leftConnections[i] * this.layer.prevLayer.neurons[i].value;
             }
             this.value = 1/(1 + Math.Exp(sum*(-1)));
         }
+
+        public void ErrorCalculation(){
+            double sum = 0;
+
+            if (this.rightConnections == null){
+                this.wrongValue = Layer.correctValues[this.count] - this.value;
+                return;
+            }else{
+                for (int i = 0;i < this.layer.nextLayer.length;i++){
+                    sum += this.layer.nextLayer.neurons[i].wrongValue * this.rightConnections[i];
+                }
+            }
+
+            this.wrongValue = sum; 
+            
+        }
+
     }
 
 }
